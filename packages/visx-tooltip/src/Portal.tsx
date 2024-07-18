f85cdba8804ddf040507342e003d4f41be7ceef6
tschai-yim <89387771+tschai-yim@users.noreply.github.com>
2024-07-18 09:42:17 +0200

    fix(tooltip): hide portal overflow

--- packages/visx-tooltip/src/Portal.tsx
+++ packages/visx-tooltip/src/Portal.tsx
@@ -4,6 +4,11 @@ import ReactDOM from 'react-dom';
 export type PortalProps = {
   /** Optional z-index to set on the Portal. */
   zIndex?: number | string;
+  /**
+   * If elements that go beyond the existing `body` extent should be clipped.
+   * @default true
+   */
+  hideOverflow?: boolean;
   /** Content to render in the Portal. */
   children: NonNullable<React.ReactNode>;
 };
@@ -23,6 +28,15 @@ export default class Portal extends React.PureComponent<PortalProps> {
     // SSR check
     if (!this.node && typeof document !== 'undefined') {
       this.node = document.createElement('div');
+      if (this.props.hideOverflow !== false) {
+        this.node.style.position = 'absolute';
+        this.node.style.top = '0';
+        this.node.style.height = '100%';
+        this.node.style.width = '100%';
+        this.node.style.overflow = 'hidden';
+        this.node.style.pointerEvents = 'none';
+        this.node.style.touchAction = 'none';
+      }
       if (this.props.zIndex != null) this.node.style.zIndex = `${this.props.zIndex}`;
       document.body.append(this.node);
     }
